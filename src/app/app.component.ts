d1909ba559538ce4dffe309fe462061f87eec7a4
mariam <mariemsafwat20@gmail.com>
2024-07-31 20:58:26 +0300

    Build a skills section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { AboutComponent } from "./about/about.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, AboutComponent],
+  imports: [RouterOutlet, HeroSectionComponent, AboutComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
